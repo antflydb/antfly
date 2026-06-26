8b1521396d66c5fa22cb5cbb12a941aab06fdb1e
AJ Roetker <ajroetker@antfly.io>
2026-06-26 03:15:42 -0700

    Remove stale SQL row lock unsupported kind

--- zig/pkg/antfly/src/sql/ddl_plan.zig
+++ zig/pkg/antfly/src/sql/ddl_plan.zig
@@ -10539,7 +10539,6 @@ fn generatedUnsupportedExpectedReason(kind: generated_parser.GeneratedSqlUnsuppo
         .reindex => .reindex_not_planned_by_generated_parser,
         .release => .release_not_planned_by_generated_parser,
         .revoke => .revoke_not_planned_by_generated_parser,
-        .read_row_lock => .read_row_lock_not_planned_by_generated_parser,
         .savepoint => .savepoint_not_planned_by_generated_parser,
         .security_label => .security_label_not_planned_by_generated_parser,
         .drop_materialized_view => .drop_materialized_view_not_planned_by_generated_parser,
--- zig/pkg/antfly/src/sql/tokenized.zig
+++ zig/pkg/antfly/src/sql/tokenized.zig
@@ -2561,7 +2561,6 @@ fn generatedUnsupportedUsesDdlPlanBoundary(kind: generated_parser.GeneratedSqlUn
         .import_foreign_schema,
         .load,
         .move,
-        .read_row_lock,
         .refresh,
         .reassign_owned,
         .security_label,
