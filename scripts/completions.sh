161a1cd0fd4307da9d809043dd261d583ad38e8b
AJ Roetker <ajroetker@antfly.io>
2026-07-08 08:07:15 -0700

    Expose release completions generation

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf completions
 mkdir completions
 echo "building antfly for shell completions"
-go build -o completions/antfly-completions ./cmd/antfly
+go build -tags "${ANTFLY_COMPLETIONS_TAGS:-afrelease}" -o completions/antfly-completions ./cmd/antfly
 # https://carlosbecker.com/posts/golang-completions-cobra/
 for sh in bash zsh fish; do
 	echo "generating $sh completion"
