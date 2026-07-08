a11fa5472163284947b65ea2af2686dd9d4a1d28
AJ Roetker <ajroetker@antfly.io>
2026-07-07 22:05:35 -0700

    Speed up release completions hook

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,7 +3,9 @@
 set -e
 rm -rf completions
 mkdir completions
+go build -o completions/antfly-completions ./cmd/antfly
 # https://carlosbecker.com/posts/golang-completions-cobra/
 for sh in bash zsh fish; do
-	go run ./cmd/antfly completion "$sh" >"completions/antfly.$sh"
+	./completions/antfly-completions completion "$sh" >"completions/antfly.$sh"
 done
+rm -f completions/antfly-completions
