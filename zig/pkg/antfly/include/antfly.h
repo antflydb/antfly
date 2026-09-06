c89dc56bf29c0e4bf8f0a1cd40b7c40419eae4fb
AJ Roetker <ajroetker@antfly.io>
2026-09-05 17:16:40 -0700

    chore: normalize licenses on changed sources

--- zig/pkg/antfly/include/antfly.h
+++ zig/pkg/antfly/include/antfly.h
@@ -5,6 +5,12 @@
 // the Elastic License 2.0 at
 //
 //     https://www.antfly.io/licensing/ELv2-license
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Elastic License 2.0 is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// Elastic License 2.0 for the specific language governing permissions and
+// limitations.
 
 #ifndef ANTFLY_H
 #define ANTFLY_H
--- zig/pkg/antfly/src/storage/ha/effects.zig
+++ zig/pkg/antfly/src/storage/ha/effects.zig
@@ -6,10 +6,10 @@
 //
 //     https://www.antfly.io/licensing/ELv2-license
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the Elastic License 2.0 is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
-// the Elastic License 2.0 for the specific language governing permissions and
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Elastic License 2.0 is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// Elastic License 2.0 for the specific language governing permissions and
 // limitations.
 
 //! Adapters from committed DB effects into the HA replication stream.
